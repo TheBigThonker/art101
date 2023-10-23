42a2ded3dc458099faf394139757e46312a218cc
TheBigThonker <pardavehl@gmail.com>
2023-10-23 00:44:23 -0700

    Lab 4 and Misc
    Lab 4 html and some changes made for practice/improvement

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,15 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Rock Paper Scissors - Pseudocode for Lab 4.
+// Author: Parker Ehlers & Cody Proctor
+// Date: 10/22/2023
+//Pseudocode for Rock, Paper, Scissors:
+  //Get all the variables: Computer choice, Player choice, and result
+  //Assign each choice a number 1 = rock, 2 = paper, 3 = scissors 
+  //Have player choose 1, 2, or 3
+  //Have computer generate a random number from 1 to 3
+  //To determine the winner:
+	  //1. If the player’s choice is the same as the computers choice then the result is a tie. 
+	  //2. If the player’s choice is 3 (scissors) and the computers choice is 2 (paper) the result is player wins.
+	  //3. If player choice is 2 (paper) and the computers choice is 1 (rock) then the result is player wins 
+	  //4. If the player choice is 1 (rock) and the computer choice is 3 (scissors)the result is player wins 
+	  //5. If any other combination occurs, the result is that the computer wins
+  //Print result
