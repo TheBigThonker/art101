e1d1385ce2ea186578cb52a7f853b824b8ab9040
TheBigThonker <pardavehl@gmail.com>
2023-11-05 21:59:59 -0800

    Lab8
    Lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,32 @@
+// Lab 8 - Anon Functions and Callbacks
+// Author: Parker Ehlers
+// Date: 5 November 2023
+
+//creating an array and a variable, then showcasing it. 
+var myArray = [7, 13, 202, 1024];
+console.log("My array: ", myArray);
+
+
+//now, creating the function
+function multiply (x, y) {
+    console.log("Numbers received: ", x, y);
+    var product = x * y;
+    return product
+}
+//testing with a few numbers
+result = multiply(4,9);
+console.log("4 * 9 = ", );
+
+result = multiply(2,8);
+console.log("2 * 8 = ", );
+
+//now, testing function with the array.
+console.log("Now, testing the array.")
+myArray.map(multiply);
+
+//next, running/creating the callback function
+myArray.map(function(x) {
+    console.log("Callback:numbers received: ", x);
+    return x / 1.5;
+}
+);
