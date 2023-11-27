19abc1d274cc17d443dd136a9e6eecede2370271
TheBigThonker <pardavehl@gmail.com>
2023-11-26 22:05:55 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,26 +7,27 @@
 // Create the FizzBuzz program itself
     for (var index=1; index<=200; index++){
         var longString, index;
-    if (index % 3 == 0) { 
-        longString += "Fizz!<br>"
+    if (index % 105 == 0) { 
+            longString += "FizzBuzzBoom!<br>"
     }
-    else if (index % 5 == 0) { 
-        longString += "Buzz!<br>"
-    }
-    else if (index % 7 == 0) { 
-        longString += "Boom!<br>"
-    }
-    else if (index % 15 == 0) { 
-        longString += "FizzBuzz!<br>"
+    //has to go from greatest to least to function properly I think
+    else if (index % 35 == 0) { 
+        longString += "BuzzBoom!<br>"
     }
     else if (index % 21 == 0) { 
         longString += "FizzBoom!<br>"
     }
-    else if (index % 35 == 0) { 
-        longString += "BuzzBoom!<br>"
+    else if (index % 15 == 0) { 
+        longString += "FizzBuzz!<br>"
     }
-    else if (index % 105 == 0) { 
-        longString += "FizzBuzzBoom!<br>"
+    else if (index % 7 == 0) { 
+        longString += "Boom!<br>"
+    }
+    else if (index % 5 == 0) { 
+        longString += "Buzz!<br>"
+    }
+    else if (index % 3 == 0) { 
+        longString += "Fizz!<br>"
     }
     else {longString += index + "<br>";
     }
